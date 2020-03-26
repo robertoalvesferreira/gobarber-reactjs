ec0148a601bffce497ecb65d612cbeece647ab98
robertoalvesferreira <robertoaff06@gmail.com>
2020-03-26 20:59:45 -0300

    Autenticação de usuario

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import history from '../../../services/history';
+import api from '../../../services/api';
+import { signInSuccess } from './actions';
 
-export default all([]);
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+  // const headers = { 'Content-Type': 'application/json' };
+  const response = yield call(api.post, 'sessions', { email, password });
+  console.tron.log(email, password);
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Ususario nao e prestador');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
